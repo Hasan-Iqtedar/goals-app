22ccf4dc9cfb918c22f0722a220906b7dab1f489
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2021-07-24 21:54:28 +0500

    Added styles and functionality to ouput goals on console initially

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const updateEnteredText = (newText) => setEnteredGoal(newText);
+  const addNewGoal = () => console.log(enteredGoal);
+
   return (
 
     <View style={styles.screen}>
+      
       <Text style={styles.title}>Goals App</Text>
+
+      <View style={styles.inputContainer}>
+
+        <TextInput style={styles.inputField} placeholder="Enter a Goal" onChangeText={updateEnteredText}/>
+        <Button title = 'Add' onPress={addNewGoal}/>
+
+      </View>
+
     </View>
 
 
@@ -23,6 +37,18 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: 'center',
     paddingBottom: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  inputField: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 7
   }
+  
 
 });
