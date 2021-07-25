7965802aec7b9123839ba895c8f21beae75b61ba
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2021-07-25 16:52:13 +0500

    Added functionality to output goals

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
+  //To keep track of entered goal.
   const [enteredGoal, setEnteredGoal] = useState('');
+  //To maintain a list of goals.
+  const [goalsList, append] = useState([]);
 
-  const updateEnteredText = (newText) => setEnteredGoal(newText);
-  const addNewGoal = () => console.log(enteredGoal);
+  /**A function to synchronize enteredGoal variable with the entered text.*/
+  const updateEnteredText = (newText) => {
+    setEnteredGoal(newText);
+  }
+
+  /**A function to append the new goal to goalsList upon clicking Add button.*/
+  const addNewGoal = () => {
+    append(currentGoals => { return [...currentGoals, enteredGoal]; });
+  }
 
   return (
 
     <View style={styles.screen}>
-      
+
       <Text style={styles.title}>Goals App</Text>
 
       <View style={styles.inputContainer}>
-
-        <TextInput style={styles.inputField} placeholder="Enter a Goal" onChangeText={updateEnteredText}/>
-        <Button title = 'Add' onPress={addNewGoal}/>
-
+        <TextInput style={styles.inputField} placeholder="Enter a Goal" onChangeText={updateEnteredText} />
+        <Button title='Add' onPress={addNewGoal} />
       </View>
 
-    </View>
+      <ScrollView>
+        {goalsList.map((goal) => {
+          return <TouchableOpacity><View key={goal} style={styles.goalsList}><Text >{goal}</Text></View></TouchableOpacity>
+        })}
+      </ScrollView>
 
+    </View>
 
   );
 }
 
 const styles = StyleSheet.create({
- 
+
   screen: {
     padding: 50
   },
@@ -48,7 +61,13 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 7
+  },
+  goalsList: {
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5,
+    backgroundColor: 'lightgray'
   }
-  
 
 });
